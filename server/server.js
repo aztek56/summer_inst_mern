f8d4890b528506c7b885caaf0f1716379e73a312
Gabriel Castillo <gcastillo@ucdavis.edu>
2018-07-02 16:05:08 -0700

    Added the routes and a test api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 // import the express server.
 const express = require('express');
+// Add the files that contain the routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 // The actual creation of the server
 const app = express();
 
 // This configures the web server root path
 app.get('/', (req, res) => res.send('Hello!'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 // This is a variable to set the number port
 const port = process.env.PORT || 5000;
